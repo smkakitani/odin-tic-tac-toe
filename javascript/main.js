6d0903f854c0e43e9c9d649051f5ed66b3bf4f49
kakisaura <82690252+kakisaura@users.noreply.github.com>
2023-01-18 23:00:37 -0300

    Create base for the game

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,38 @@
-const gameboard = () => {
+const gameboard = (() => { //gameboard object
   const gameboard = [];
+  const displayController = [];
+  const displayBoard = document.getElementById('board-display');
 
-}
+  // creating the board
+  const createBoard = () => {
+    let i = 0;
+    while (i < 9) {
+      const displayBox = document.createElement('div');
+      const para = document.createElement('p');
 
+      displayBox.classList.add('box');
+      displayBox.setAttribute('data-index', `${i}`);
+      // displayBox.textContent = "X";
+      displayBoard.appendChild(displayBox);
+
+      para.textContent = "O";
+      displayBox.appendChild(para);
+      i++;
+    }
+  };
+
+  //event listener
+  
+  
+
+  return {
+    createBoard,
+
+  }
+})();
+gameboard.createBoard();
+
+// factory function to create player
 const createPlayer = (player, symbol ) => {
   return {
     player,
@@ -12,3 +42,5 @@ const createPlayer = (player, symbol ) => {
 
 const playerOne = createPlayer('p1', 'X');
 const playerTwo = createPlayer('p2', 'O');
+
+
